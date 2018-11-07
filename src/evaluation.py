added evaluation masking fixed bugs in selection methods

src/evaluation.py
@@ -24,33 +24,40 @@ class Evaluation:
             print("Euclidean distance method selected for evaluation")
         else:
             raise Exception("Incorrect method selected for evaluation")
+
     # todo incorporate mutation masking to save time (only calculate fitness for individuals we mutated)
-    def evaluate(self):
+    def evaluate(self, use_mask=False):
         start_timer("evaluation")
-        fitness = self.method(self.tsp.population)
-        self.tsp.fitness = fitness
+        if not use_mask:
+            self.tsp.fitness = self.method(self.tsp.population)
+        # else we only evaluate individuals who are indicated to be evaluated
+        elif self.tsp.mutant_index.size != 0:
+            self.tsp.fitness[self.tsp.mutant_index] = self.method(self.tsp.population[self.tsp.mutant_index])
         add_timer("evaluation")
 
-    def evaluate_children(self):
+    def evaluate_children(self, use_mask=False):
         start_timer("evaluation")
-        fitness = self.method(self.tsp.children)
-        self.tsp.children_fitness = fitness
+        if not use_mask:
+            self.tsp.children_fitness = self.method(self.tsp.children)
+        elif self.tsp.mutant_children_index.size != 0:
+            self.tsp.children_fitness[self.tsp.mutant_children_index] = self.method(
+                self.tsp.children[self.tsp.mutant_children_index])
         add_timer("evaluation")
 
-    def euclidean(self,a,b):
+    def euclidean(self, a, b):
         """
         euclidean distance
         """
-        if a>b:
-            a,b = b,a
-        return self.data[a,b]
+        if a > b:
+            a, b = b, a
+        return self.data[a, b]
 
     # Need to use an efficient implementation for this
     def use_preprocessed_array(self, population):
         distance = []
         for individual in population:
             current_distance = 0
-            for a in range(len(individual)-1):
-                current_distance -= self.euclidean(individual[a], individual[a+1])
+            for a in range(len(individual) - 1):
+                current_distance -= self.euclidean(individual[a], individual[a + 1])
             distance.append(current_distance)
         return np.array(distance)

src/parent_selection.py
@@ -20,13 +20,20 @@ class Parent_Selection:
         if method_str.lower() == "random":
             self.method = self.random
             print("Random method selected for parent selection")
+        elif method_str.lower() == "mu_plus_lambda":
+            self.method = self.mu_plus_lambda
+            print("mu+lambda method selected for survivor selection")
         else:
             raise Exception("Incorrect method selected for parent selection")
 
     def select(self):
         start_timer("parent selection")
-        self.tsp.parent_index = self.method()
+        self.method()
         add_timer("parent selection")
 
     def random(self):
-        return np.random.choice(self.tsp.population_size, self.tsp.num_parents, replace=False)
+        self.tsp.parent_index = np.random.choice(self.tsp.population_size, self.tsp.num_parents, replace=False)
+
+    def mu_plus_lambda(self):
+        args = np.argsort(self.tsp.fitness)[::-1]
+        self.tsp.parent_index = args[:self.tsp.num_parents]

src/survivor_selection.py
@@ -33,12 +33,13 @@ class Survivor_Selection:
     def random(self):
         combo_pop = np.append(self.tsp.population, self.tsp.children, axis=0)
         combo_fitness = np.append(self.tsp.fitness, self.tsp.children_fitness)
-        selection = np.random.randint(self.tsp.population.shape[0], size=self.population.shape[0])
+        selection = np.random.randint(self.tsp.population.shape[0], size=self.tsp.population.shape[0])
         self.tsp.population = combo_pop[selection]
         self.tsp.fitness = combo_fitness[selection]
 
     def mu_plus_lambda(self):
         combo_pop = np.append(self.tsp.population, self.tsp.children, axis=0)
         combo_fitness = np.append(self.tsp.fitness, self.tsp.children_fitness)
-        args = np.argsort(combo_fitness)
-        return combo_pop[args][-self.tsp.population.shape[0]:], combo_fitness[args][-self.tsp.population.shape[0]:]
+        args = np.argsort(combo_fitness)[::-1]
+        self.tsp.population = combo_pop[args][:self.tsp.population_size]
+        self.tsp.fitness = combo_fitness[args][:self.tsp.population_size]

src/travelling_salesman.py
@@ -20,18 +20,20 @@ from src.utils import debug_print as print
 from src.file_utils import parse_file as parse
 
 # CONSTS
-POP_SIZE = 50
+POP_SIZE = 100
 # STR_LENGTH = 10
-NUM_PARENTS = 10
-NUM_GENERATIONS = 100
+NUM_PARENTS = 100
+NUM_GENERATIONS = 250
 TIME_LIMIT = 100000
-MUTATION_RATE = .05
+MUTATION_RATE = .2
 INIT_METHOD = "random_permutations"
-SELECT_METHOD = "random"
+# SELECT_METHOD = "random"
+SELECT_METHOD = "mu_plus_lambda"
 # CROSSOVER_METHOD = "cut_and_crossfill"
 CROSSOVER_METHOD = "order_crossover"
 MUTATION_METHOD = "swap"
 EVALUATION_METHOD = "precalculate_distances"
+# SURVIVOR_METHOD = "random"
 SURVIVOR_METHOD = "mu_plus_lambda"
 TERMINATOR_METHOD = "num_iterations"
 DEBUG = True
@@ -80,7 +82,7 @@ def the_tsp_problem():
         mutator.mutate_population()
         mutator.mutate_children()
         # re-evaluate children and population
-        evaluator.evaluate()
+        evaluator.evaluate(use_mask=True)
         evaluator.evaluate_children()
         # select from parents and children to form new population
         survivor_selector.select()
@@ -97,8 +99,8 @@ def the_tsp_problem():
 
     # finished, print results
     print("*" * 20)
-    print("Final Mean Fitness: {}\t Best Fitness:{}".format(tsp.fitness.mean(), tsp.fitness.max()))
     print("Best Member of Population:\n", tsp.population[np.argmax(tsp.fitness)])
+    print("Final Mean Fitness: {}\t Best Fitness:{}".format(tsp.fitness.mean(), tsp.fitness.max()))
     print("*" * 10 + "\nFunction Times (in ms):\n")
     for k, v in get_times():
         print("{:16}\t{:.2f}".format(k, v * 1000))