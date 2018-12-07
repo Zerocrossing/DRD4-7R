a0f555036726e1087f2d56f72a47bf388769898c
Vineel Nagisetty <vineel.nagisetty@gmail.com>
2018-12-07 17:10:38 -0330

    Cut and Crossfill works with passing all parents at once (similar to order crossover). No numba love though

--- src/recombination.py
+++ src/recombination.py
@@ -22,7 +22,7 @@ class Recombination:
         :return:
         """
         if method_str.lower() == "cut_and_crossfill":
-            self.method = self.cut_and_crossfill
+            self.method = cut_and_crossfill
             print("Cut and crossfill method selected for recombination")
         elif method_str.lower() == "order_crossover":
             self.method = order_crossover
@@ -43,24 +43,34 @@ class Recombination:
         self.tsp.children = self.method(parents, self.dist_cache)
         add_timer("recombination")
 
-    def cut_and_crossfill(self, parent1, parent2):
-        """
-        cut and crossfill method
-        Note: ONLY works with permutations, not random strings
-        """
-        cut = np.random.randint(1, len(parent1) - 1)
-        offspring1, offspring2 = np.empty(len(parent1), int), np.empty(len(parent2), int)
-        offspring1.fill(-1)
-        offspring2.fill(-1)
-        offspring1[:cut] = parent1[:cut]
-        offspring2[:cut] = parent2[:cut]
-        while -1 in offspring1 or -1 in offspring2:
-            if parent2[cut] not in offspring1:
-                offspring1[np.nonzero(offspring1 == -1)[0][0]] = parent2[cut]  # index of first nonzero
-            if parent1[cut] not in offspring2:
-                offspring2[np.nonzero(offspring2 == -1)[0][0]] = parent1[cut]
-            cut = (cut + 1) % len(parent1)
-        return offspring1, offspring2
+def cut_and_crossfill(parents, _):
+    children = np.empty_like(parents, dtype=np.uint16)
+    for n in prange(parents.shape[0] // 2):
+        p1 = parents[n * 2]
+        p2 = parents[n * 2 + 1]
+        c1, c2 = cut_and_crossfill_xover(p1, p2)
+        children[n * 2] = c1
+        children[n * 2 + 1] = c2
+    return children
+
+def cut_and_crossfill_xover(parent1, parent2):
+    """
+    cut and crossfill method
+    Note: ONLY works with permutations, not random strings
+    """
+    cut = np.random.randint(1, len(parent1) - 1)
+    offspring1, offspring2 = np.empty(len(parent1), int), np.empty(len(parent2), int)
+    offspring1.fill(-1)
+    offspring2.fill(-1)
+    offspring1[:cut] = parent1[:cut]
+    offspring2[:cut] = parent2[:cut]
+    while -1 in offspring1 or -1 in offspring2:
+        if parent2[cut] not in offspring1:
+            offspring1[np.nonzero(offspring1 == -1)[0][0]] = parent2[cut]  # index of first nonzero
+        if parent1[cut] not in offspring2:
+            offspring2[np.nonzero(offspring2 == -1)[0][0]] = parent1[cut]
+        cut = (cut + 1) % len(parent1)
+    return offspring1, offspring2
 
 
 """Order Crossover"""
